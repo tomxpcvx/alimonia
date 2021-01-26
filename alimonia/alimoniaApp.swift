1261c73a3ec7619415b1741c62803fdbf1762c0a
tomxpcvx <dev@tomxpcvx.wtf>
2021-01-26 12:25:02 +0100

    🎉 Initial Commit

---
+++ alimonia/alimoniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  alimoniaApp.swift
+//  alimonia
+//
+//  Created by Tom Pietsch on 26.01.21.
+//
+
+import SwiftUI
+
+@main
+struct alimoniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
